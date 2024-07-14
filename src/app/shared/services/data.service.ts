bb11d9d01db7ea129ba63aebe0d20d4674a261d2
mohamedfawzyy <mohamedfawzzy6@gmail.com>
2024-07-14 06:51:31 +0300

    try local server

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -10,10 +10,10 @@ export class DataService {
   constructor(private _HttpClient:HttpClient) { }
 
   getAllCustomers():Observable<any>{
-    return this._HttpClient.get(`http://localhost:3000/customers`);
+    return this._HttpClient.get(`https://my-json-server.typicode.com/mohamedfawzyy/Route_Angular_Task/customers`);
   }
   getAllTransactions():Observable<any>{
-    return this._HttpClient.get(`http://localhost:3000/transactions`);
+    return this._HttpClient.get(`https://my-json-server.typicode.com/mohamedfawzyy/Route_Angular_Task/transactions`);
   }
 }
 
